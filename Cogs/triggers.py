a9029d6076336e4aff540a7ad47f5a8c12f6f310
coolical <45604130+coolical@users.noreply.github.com>
2020-04-22 09:19:24 -0500

    Update triggers.py

--- Cogs/triggers.py
+++ Cogs/triggers.py
@@ -7,7 +7,7 @@ discordPlaylistEmbed.add_field(name="Spotify Playlist: ", value="https://open.sp
 discordPlaylistEmbed.add_field(name="Submission Form: ", value="https://goo.gl/forms/SBzO6tlR99fzPBJb2")
 discordPlaylistEmbed.add_field(name="Results: ", value="https://docs.google.com/spreadsheets/d/1yprWT1_eihGmfkwLQKFXztQ7yrF-UXYS5vmHjFyEypw/edit?usp=sharing")
 triggers = [{"name":"discord playlist", "reply": [discordPlaylistEmbed], "description": "displays information related to discord playlist", "typeWait": False, "prob": 100},
- {"name":"morning", "reply": "Good morning! Anyone up for a morning put?", "description": "I'll sometimes reply to a morning greeting", "typeWait": True, "prob": 50}]#prob must be [0,100] 
+ {"name":"morning", "reply": ["Good morning! Anyone up for a morning put?"], "description": "I'll sometimes reply to a morning greeting", "typeWait": True, "prob": 50}]#prob must be [0,100] 
 for trigger in triggers:
     try:
         conf.triggers.append({"key":trigger["name"], "value":trigger["description"]})
