0b4ebdc6bda49c889918f440ccdd3fea4e3002a6
coolical <45604130+coolical@users.noreply.github.com>
2020-04-22 07:34:40 -0500

    Update help.py

--- Cogs/help.py
+++ Cogs/help.py
@@ -7,10 +7,12 @@ class Help(client.Cog):
         self.b=bot
     @client.command(name='help', description='displays help text for commands', help=f"'{conf.prefix1}help <command name>' or '{conf.prefix1}help'")
     async def help(self,ctx,arg="NONE"):
-        embed = discord.Embed(title="Help", color=conf.color)
+        embed = discord.Embed(title="Help",description="Hello! I'm Arnold Palmer! Coolical dug up my dead body and injected my .chr file into discord! Here are the things I can do", color=conf.color)
         if arg=="NONE":#sends either general help or specified help
             for command in self.b.commands:
-                embed.add_field(name=command.name, value=command.help)
+                embed.add_field(name=command.name, value=command.description)
+            for trigger in conf.triggers:
+                embed.add_field(name=trigger["name"], value=trigger['description'])
         else:            
             try:
                 command = self.b.get_command(arg)
