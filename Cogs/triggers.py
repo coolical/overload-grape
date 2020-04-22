8bb6d1ba2a56d4fc1406ff85ad022d8a7e32b6e3
coolical <45604130+coolical@users.noreply.github.com>
2020-04-22 07:01:34 -0500

    Create triggers.py

---
+++ Cogs/triggers.py
@@ -0,0 +1,33 @@
+import discord, random, asyncio, random
+from discord.ext import commands as client
+from Cogs.config import conf
+discordPlaylistEmbed= discord.Embed(title="Discord Playlist", color=conf.color)
+discordPlaylistEmbed.add_field(name="Youtube Playlist: ", value="https://www.youtube.com/playlist?list=PLZCHV2UiCyWrc-bEgcsYLTP2sFd-w0C2p")
+discordPlaylistEmbed.add_field(name="Spotify Playlist: ", value="https://open.spotify.com/user/coolical/playlist/3d4iTU9IygolOxnQjjtwLE?si=HUCWDXQnTx-XAhu-uTdc4Q")
+discordPlaylistEmbed.add_field(name="Submission Form: ", value="https://goo.gl/forms/SBzO6tlR99fzPBJb2")
+discordPlaylistEmbed.add_field(name="Results: ", value="https://docs.google.com/spreadsheets/d/1yprWT1_eihGmfkwLQKFXztQ7yrF-UXYS5vmHjFyEypw/edit?usp=sharing")
+triggers = [{"name":"discord playlist", "reply": discordPlaylistEmbed, "description": "displays information related to discord playlist"}]
+for trigger in triggers:
+    try:
+        conf.triggers.append({trigger.name, trigger.description})
+    except:
+        pass
+
+class Triggers(client.Cog):
+    def __init__(self, bot):
+        self.b = bot
+
+    @client.Cog.listener()
+    async def on_message(self, message):
+        for trigger in triggers:
+            if trigger['name'] in message.content.lower() and not message.author.bot:#checks if bot and if trigger is said
+                async with message.channel.typing():
+                    await asyncio.sleep(conf.type_speed)
+                chosen = random.choice('choices')
+                if isinstance(chosen, str):#sends choice as repective type
+                    await message.channel.send(trigger['reply'])
+                else:
+                    await message.channel.send(embed=trigger['reply'])
+
+def setup(bot):
+    bot.add_cog(Triggers(bot))
