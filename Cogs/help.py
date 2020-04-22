5a7ace74a217481c894e744247b284d9f8af0460
coolical <45604130+coolical@users.noreply.github.com>
2020-04-22 09:02:17 -0500

    fixed help, added trigger
    fixed help relevent help desription, added morning trigger

--- Cogs/help.py
+++ Cogs/help.py
@@ -7,14 +7,15 @@ class Help(client.Cog):
         self.b=bot
     @client.command(name='help', description='displays help text for commands', help=f"'{conf.prefix1}help <command name>' or '{conf.prefix1}help'")
     async def help(self,ctx,arg="NONE"):
-        embed = discord.Embed(title="Help",description="Hello! I'm Arnold Palmer! Coolical dug up my dead body and injected my .chr file into discord! Here are the things I can do", color=conf.color)
         if arg=="NONE":#sends either general help or specified help
+            embed = discord.Embed(title="Help",description="Hello! I'm Arnold Palmer! Coolical dug up my dead body and injected my .chr file into discord! Here are the things I can do", color=conf.color)
             for command in self.b.commands:
                 embed.add_field(name=f"{conf.prefix1}{command.name}", value=command.description)
             for trigger in conf.triggers:
                 embed.add_field(name=trigger["key"], value=trigger['value'])
         else:            
             try:
+                embed = discord.Embed(title="Help",description="Here is the usage", color=conf.color)
                 command = self.b.get_command(arg)
                 embed.add_field(name=command.name, value=command.help)
             except:
--- Cogs/triggers.py
+++ Cogs/triggers.py
@@ -6,7 +6,8 @@ discordPlaylistEmbed.add_field(name="Youtube Playlist: ", value="https://www.you
 discordPlaylistEmbed.add_field(name="Spotify Playlist: ", value="https://open.spotify.com/user/coolical/playlist/3d4iTU9IygolOxnQjjtwLE?si=HUCWDXQnTx-XAhu-uTdc4Q")
 discordPlaylistEmbed.add_field(name="Submission Form: ", value="https://goo.gl/forms/SBzO6tlR99fzPBJb2")
 discordPlaylistEmbed.add_field(name="Results: ", value="https://docs.google.com/spreadsheets/d/1yprWT1_eihGmfkwLQKFXztQ7yrF-UXYS5vmHjFyEypw/edit?usp=sharing")
-triggers = [{"name":"discord playlist", "reply": [discordPlaylistEmbed], "description": "displays information related to discord playlist", "typeWait": False}]
+triggers = [{"name":"discord playlist", "reply": [discordPlaylistEmbed], "description": "displays information related to discord playlist", "typeWait": False, "prob": 100},
+ {"name":"morning", "reply": "Good morning! Anyone up for a morning put?", "description": "I'll sometimes reply to a morning greeting", "typeWait": True, "prob": 50}]#prob must be [0,100] 
 for trigger in triggers:
     try:
         conf.triggers.append({"key":trigger["name"], "value":trigger["description"]})
@@ -20,7 +21,7 @@ class Triggers(client.Cog):
     @client.Cog.listener()
     async def on_message(self, message):
         for trigger in triggers:
-            if trigger['name'] in message.content.lower() and not message.author.bot:#checks if bot and if trigger is said
+            if trigger['name'] in message.content.lower() and not message.author.bot and  random.randint(0,100) <= trigger["prob"]:#checks if bot and if trigger is said and if it can be said based on given probability
                 if trigger['typeWait'] == True:
                     async with message.channel.typing():
                         await asyncio.sleep(conf.type_speed)
